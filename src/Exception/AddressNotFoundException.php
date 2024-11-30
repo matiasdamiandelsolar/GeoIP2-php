69d800c78b1c5ee99ec72550f213c7c5ecafb298
lifegame1lu111 <lucaskunzpedroso@gmail.com>
2024-11-29 23:54:53 -0300

    Xd

---
+++ src/Exception/AddressNotFoundException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace matiasdamian\matiasdamian\GeoIp2\Exception;
+
+/**
+ * This class represents a generic error.
+ */
+// phpcs:disable
+class AddressNotFoundException extends GeoIp2Exception {}
